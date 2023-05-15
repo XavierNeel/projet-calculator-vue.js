33b6720af2f5495812007db753bd9398f52bd97e
neel <xavierneel@gmail.com>
2023-05-15 15:19:46 +0200

    calculator Project Html Css

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div id="app">
+    <Calculator></Calculator>
+
+  </div>
+
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+
+</script>
+
+
+
+<style>
+@import url('./assets/main.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
+
+#app {
+  font-family: 'Roboto', sans-serif;
+}
+</style>
